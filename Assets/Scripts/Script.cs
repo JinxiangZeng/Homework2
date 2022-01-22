cc818b70dbbb026bd3ad798f43b5c8d26dcb0054
Jinxiang Zeng <jzeng1@my.westga.edu>
2022-01-21 20:59:06 -0500

    Implemented function to increase size of clicked object, and previouse one return to origin

--- Assets/Scripts/Script.cs
+++ Assets/Scripts/Script.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnMouseDown()
     {
-        
-    }
+        Renderer renderer = GetComponent<Renderer>();
+        transform.localScale = new Vector3(1f, 0.6f, 0.87f);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject[] cylinderObjects = GameObject.FindGameObjectsWithTag("Cylinder");
+        foreach (GameObject cylinderObject in cylinderObjects)
+        {
+            Renderer cylinder = cylinderObject.GetComponent<Renderer>();
+            if (cylinder.material.color != renderer.material.color)
+            {
+                cylinder.transform.localScale = new Vector3(1f, 0.29f, 0.87f);
+            }
+        }
     }
 }
